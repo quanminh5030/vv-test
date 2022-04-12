46e9bf50f32ff948f690371e50117a81e4cdbe8e
Quan Dao <quan.dao912@gmail.com>
2022-04-13 00:20:10 +0300

    modified img path in server & create carousel in client site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,91 @@
-import React from 'react';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      hello world
-    </div>
-  );
+import './App.css'
+
+interface DataType {
+  case1: {
+    one: string
+    two: string
+    three: string
+  }
+  case2: {
+    zero: string
+    one: string
+    two: string
+    three: string
+  }
 }
 
-export default App;
+type SelectedType = 'case1' | 'case2'
+
+function App() {
+  const [data, setData] = useState<DataType | null>(null)
+  const [selected, setSelected] = useState<SelectedType>('case1')
+  const [degree, setDegree] = useState(0)
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/all')
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  if (!data) {
+    return null
+  }
+
+  const handleClick = (value: SelectedType) => {
+    setSelected(value)
+    setCurrent(0)
+  }
+
+  const handleRotate = () => {
+    setDegree(degree + 90)
+  }
+
+  const handleChange = (current: any) => {
+    setCurrent(current)
+  }
+
+  return (
+    <div className="App">
+      <div style={{ margin: '50px' }}>
+        {Object.keys(data).map((key) => (
+          <button
+            key={key}
+            style={key === selected ? { background: 'lightblue' } : {}}
+            onClick={() => handleClick(key as SelectedType)}
+          >
+            {key}
+          </button>
+        ))}
+      </div>
+
+      <div style={{ width: '70%', margin: 'auto' }}>
+        <Carousel
+          showArrows={true}
+          onChange={(current) => handleChange(current)}
+        >
+          {Object.values(data[selected]).map((item, index) => (
+            <div key={item}>
+              <img
+                src={item}
+                alt={item}
+                style={
+                  index === current ? { transform: `rotate(${degree}deg)` } : {}
+                }
+              />
+            </div>
+          ))}
+        </Carousel>
+        <button onClick={handleRotate}>Rotate</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,44 @@
 import express from 'express'
+import fs from 'fs'
+import yaml from 'js-yaml'
+import path from 'path'
+import cors from 'cors'
+import _ from 'lodash'
 
 const app = express()
+app.use('/static', express.static('data'))
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('be works')
+})
+
+app.get('/all', (req, res) => {
+  try {
+    const filename = path.join(__dirname, 'data.yml')
+    const data: any = yaml.load(fs.readFileSync(filename, 'utf-8'))
+
+    const updatePath = 'http://localhost:3001/static'
+
+    const case1 = data.case1
+    const case2 = data.case2
+
+    Object.keys(case1).map((key) => {
+      _.update(case1, key, function (n) {
+        return updatePath + n
+      })
+    })
+
+    Object.keys(case2).map((key) => {
+      _.update(case2, key, function (n) {
+        return updatePath + n
+      })
+    })
+
+    res.send(data)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 const PORT = 3001
